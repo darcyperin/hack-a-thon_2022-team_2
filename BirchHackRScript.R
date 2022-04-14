1f7bbf9edf58f76ea330cab5093ff71be8f2248d
Birch Maxwell Lazo-Murphy <Birch Maxwell>
2022-04-14 10:57:31 -0400

    shitty places to be

--- BirchHackRScript.R
+++ BirchHackRScript.R
@@ -6,6 +6,8 @@
 
 library(tidyverse)
 library(readxl)
+library(viridisLite)
+library(viridis)
 
 data18 <- read_xls("data/2018303d_final.xls")
 data08 <- read_xls("data/SC_303d_lists_2006to2016/2008303dfinal020608prtyrk.xls")
@@ -17,6 +19,9 @@ summary(data98)
 
 # Stations who FAILED to improve from 1998 to 2008
 stationsFail98to08 <- inner_join(data98, data08, by = "STATION")
+Failed98to08MoreThanOne <- stationsFail98to08 %>% 
+  group_by(LOCATION) %>% 
+  summarize(count=n())
 
 # Stations who FAILED to improve from 1998 to 2018
 stationsFail20yr <- inner_join(stationsFail98to08, data18, by = "STATION")
@@ -36,4 +41,60 @@ stationsWorse20yr <- data18[!data18$STATION %in% stationsWorse98to08$STATION,]
 
 # Locations that have one or more issue associated with them over the 20 year
 # period
+Failed98to18MoreThanOne <- stationsFail20yr %>% 
+  group_by(LOCATION) %>% 
+  summarize(NumCauses=n()) %>%
+  group_by(NumCauses) %>%
+  summarize(count2 = n())
+Failed98to18MoreThanOne$NumCauses <- sub("^", "N", Failed98to18MoreThanOne$NumCauses)
+Failed98to18MoreThanOne$Percent <- c(Failed98to18MoreThanOne$count2[1]/sum(Failed98to18MoreThanOne$count2)*100,
+                                     Failed98to18MoreThanOne$count2[2]/sum(Failed98to18MoreThanOne$count2)*100,
+                                     Failed98to18MoreThanOne$count2[3]/sum(Failed98to18MoreThanOne$count2)*100,
+                                     Failed98to18MoreThanOne$count2[4]/sum(Failed98to18MoreThanOne$count2)*100,
+                                     Failed98to18MoreThanOne$count2[5]/sum(Failed98to18MoreThanOne$count2)*100,
+                                     Failed98to18MoreThanOne$count2[6]/sum(Failed98to18MoreThanOne$count2)*100)
 
+ggplot(Failed98to18MoreThanOne, aes(x= "", y=Percent, fill=NumCauses)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  scale_fill_viridis(option = "C", discrete = TRUE) +
+  theme_void() +
+  labs(title = "Number of Causes for Stations Listed from 1998 to 2018")
+
+Pass98to18MoreThanOne <- stationsPass20yr %>% 
+  group_by(DESCRIPTION) %>% 
+  summarize(NumCauses=n()) %>%
+  na.omit() %>%
+  group_by(NumCauses) %>%
+  summarize(count2 = n()) 
+Pass98to18MoreThanOne$NumCauses <- sub("^", "N", Pass98to18MoreThanOne$NumCauses)
+Pass98to18MoreThanOne$Percent <- c(Pass98to18MoreThanOne$count2[1]/sum(Pass98to18MoreThanOne$count2)*100,
+                                     Pass98to18MoreThanOne$count2[2]/sum(Pass98to18MoreThanOne$count2)*100,
+                                     Pass98to18MoreThanOne$count2[3]/sum(Pass98to18MoreThanOne$count2)*100)
+                                     
+
+ggplot(Pass98to18MoreThanOne, aes(x= "", y=Percent, fill=NumCauses)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  scale_fill_viridis(option = "C", discrete = TRUE) +
+  theme_void() +
+  labs(title = "Number of Causes for Stations Removed 1998 to 2018")
+# They are doing a shit job of removing stations with more than one issue
+
+Add98to18MoreThanOne <- stationsWorse20yr %>% 
+  group_by(DESCRIPTION) %>% 
+  summarize(NumCauses=n()) %>%
+  na.omit() %>%
+  group_by(NumCauses) %>%
+  summarize(count2 = n()) 
+Add98to18MoreThanOne$NumCauses <- sub("^", "N", Add98to18MoreThanOne$NumCauses)
+Add98to18MoreThanOne$Percent <- c(Add98to18MoreThanOne$count2[1]/sum(Add98to18MoreThanOne$count2)*100,
+                                   Add98to18MoreThanOne$count2[2]/sum(Add98to18MoreThanOne$count2)*100)
+
+
+ggplot(Add98to18MoreThanOne, aes(x= "", y=Percent, fill=NumCauses)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) +
+  scale_fill_viridis(option = "C", discrete = TRUE) +
+  theme_void() +
+  labs(title = "Number of Causes for Stations Added 1998 to 2018")
